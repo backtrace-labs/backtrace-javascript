42d5cee82b551215f8a3422dc39f450ec5239448
Konrad Dysput <konrad.dysput@gmail.com>
2023-05-26 14:57:03 +0200

    Monorepo setup

---
+++ packages/core/src/BacktraceCoreClientBuilder.ts
@@ -0,0 +1,8 @@
+import { BacktraceCoreClient } from './BacktraceCoreClient';
+import { BacktraceConfiguration } from './model/configuration/BacktraceConfiguration';
+
+export class BacktraceCoreClientBuilder {
+    public static build(options?: BacktraceConfiguration): BacktraceCoreClient {
+        throw new Error('Not implemented');
+    }
+}
