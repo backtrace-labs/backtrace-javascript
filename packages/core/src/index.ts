e546ce3af148cd89600f5a8db7c88aac73037aeb
Konrad Dysput <konrad.dysput@gmail.com>
2023-05-29 15:46:28 +0200

    Removed library

--- packages/core/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './BacktraceCoreClient';
-export * from './BacktraceCoreClientBuilder';
-export * from './model/configuration/BacktraceConfiguration';
-export * from './model/configuration/BacktraceDatabaseConfiguration';
-export * from './model/report/BacktraceAttachment';
-export * from './model/report/BacktraceReport';
--- packages/core/tests/client/clientTests.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BacktraceCoreClient } from '../../src/';
-describe('Client tests', () => {
-    it('Should create a client', () => {
-        const client = new BacktraceCoreClient();
-
-        expect(client).toBeDefined();
-    });
-});
