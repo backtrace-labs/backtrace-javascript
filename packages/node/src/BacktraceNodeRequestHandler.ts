281b57b0c6685785fd7dc8422ac8b6620c4925f6
Konrad Dysput <konrad.dysput@gmail.com>
2023-06-13 17:09:36 +0200

    Do not use axios in nodejs

--- packages/node/src/BacktraceNodeRequestHandler.ts
+++ packages/node/src/BacktraceNodeRequestHandler.ts
@@ -6,17 +6,21 @@ import {
     DEFAULT_TIMEOUT,
 } from '@backtrace/sdk-core';
 import { BacktraceData } from '@backtrace/sdk-core/src/model/data/BacktraceData';
-import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
+import http from 'http';
 import https from 'https';
+
 import path from 'path';
 
 export class BacktraceNodeRequestHandler implements BacktraceRequestHandler {
     private readonly UPLOAD_FILE_NAME = 'upload_file';
     private readonly _timeout: number;
 
-    private readonly _httpAgent: https.Agent;
+    private readonly JSON_HEADERS = {
+        'Content-type': 'application/json',
+    };
+
     constructor(
         private readonly _options: {
             url: string;
@@ -26,9 +30,6 @@ export class BacktraceNodeRequestHandler implements BacktraceRequestHandler {
         },
     ) {
         this._timeout = this._options.timeout ?? DEFAULT_TIMEOUT;
-        this._httpAgent = this._options.ignoreSslCertificate
-            ? new https.Agent({ rejectUnauthorized: false })
-            : https.globalAgent;
     }
 
     public async postError(
@@ -49,27 +50,55 @@ export class BacktraceNodeRequestHandler implements BacktraceRequestHandler {
         payload: string | FormData,
     ): Promise<BacktraceReportSubmissionResult<T>> {
         try {
-            const headers = {
-                'Content-Type':
-                    typeof payload === 'string'
-                        ? 'application/json'
-                        : `multipart/form-data; boundary=${payload.getBoundary()}`,
-            };
-            const result = await axios.post<T>(submissionUrl, payload, {
-                timeout: this._timeout,
-                httpsAgent: this._httpAgent,
-                headers,
+            const url = new URL(submissionUrl);
+            const httpClient = this.getHttpClient(url);
+
+            return new Promise<BacktraceReportSubmissionResult<T>>((res) => {
+                const request = httpClient.request(
+                    {
+                        rejectUnauthorized: this._options.ignoreSslCertificate === true,
+                        timeout: this._timeout,
+                        port: url.port ?? 443,
+                        method: 'POST',
+                        headers: typeof payload === 'string' ? this.JSON_HEADERS : payload.getHeaders(),
+                    },
+                    (response) => {
+                        let result = '';
+                        response.on('data', (d) => {
+                            result += d.toString();
+                        });
+                        response.on('end', () => {
+                            switch (response.statusCode) {
+                                case 200: {
+                                    res(BacktraceReportSubmissionResult.Ok(this.parseServerResponse(result)));
+                                    break;
+                                }
+                                case 429: {
+                                    res(BacktraceReportSubmissionResult.OnLimitReached());
+                                    break;
+                                }
+                                default: {
+                                    res(BacktraceReportSubmissionResult.OnInternalServerError(result));
+                                    break;
+                                }
+                            }
+                        });
+                    },
+                );
+
+                request.on('error', (err: Error) => {
+                    return res(BacktraceReportSubmissionResult.OnInternalServerError(err.message));
+                });
+
+                if (typeof payload === 'string') {
+                    request.write(payload);
+                } else {
+                    payload.pipe(request);
+                }
+
+                request.end();
             });
-            return BacktraceReportSubmissionResult.Ok(result.data);
         } catch (err) {
-            if (axios.isAxiosError(err) && err.response) {
-                if (err.response.status === 429) {
-                    return BacktraceReportSubmissionResult.OnLimitReached();
-                }
-                if (err.response.status >= 500) {
-                    return BacktraceReportSubmissionResult.OnInternalServerError(err.message);
-                }
-            }
             const error = err as Error & { code: string };
 
             if (error.code === 'ETIMEDOUT' || error.code === 'ECONNRESET' || error.code === 'ECONNABORTED') {
@@ -81,6 +110,17 @@ export class BacktraceNodeRequestHandler implements BacktraceRequestHandler {
         }
     }
 
+    private getHttpClient(submissionUrl: URL) {
+        return submissionUrl.protocol === 'http' ? http : https;
+    }
+
+    private parseServerResponse<T>(response: string): T {
+        try {
+            return JSON.parse(response);
+        } catch {
+            return {} as T;
+        }
+    }
     private createFormData(json: string, attachments?: BacktraceAttachment[]) {
         const formData = new FormData();
         const blob = new Blob([json]);
