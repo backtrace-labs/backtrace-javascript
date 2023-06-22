fccb76b967d97ca75beabaad9a6a9375634a4d7f
Sebastian Alex <sebapotok@gmail.com>
2023-06-22 15:24:22 +0200

    Add common stuff necessary for Webpack in browser and node. (#19)
    * sdk-core package: change IdGenerator implementation to remove crypto dependency
    
    * build: add common webpack configurations
    
    ---------
    
    Co-authored-by: Sebastian Alex <sebastian.alex@saucelabs.com>

--- packages/sdk-core/src/common/IdGenerator.ts
+++ packages/sdk-core/src/common/IdGenerator.ts
@@ -1,19 +1,33 @@
-import { pseudoRandomBytes } from 'crypto';
 export class IdGenerator {
     public static uuid() {
-        const bytes = pseudoRandomBytes(16);
+        const bytes = [...new Array(16)].map(() => Math.floor(Math.random() * 256));
         bytes[6] = (bytes[6] & 0x0f) | 0x40;
         bytes[8] = (bytes[8] & 0x3f) | 0x80;
         return (
-            bytes.slice(0, 4).toString('hex') +
+            bytes
+                .slice(0, 4)
+                .map((n) => n.toString(16).padStart(2, '0'))
+                .join('') +
             '-' +
-            bytes.slice(4, 6).toString('hex') +
+            bytes
+                .slice(4, 6)
+                .map((n) => n.toString(16).padStart(2, '0'))
+                .join('') +
             '-' +
-            bytes.slice(6, 8).toString('hex') +
+            bytes
+                .slice(6, 8)
+                .map((n) => n.toString(16).padStart(2, '0'))
+                .join('') +
             '-' +
-            bytes.slice(8, 10).toString('hex') +
+            bytes
+                .slice(8, 10)
+                .map((n) => n.toString(16).padStart(2, '0'))
+                .join('') +
             '-' +
-            bytes.slice(10, 16).toString('hex')
+            bytes
+                .slice(10, 16)
+                .map((n) => n.toString(16).padStart(2, '0'))
+                .join('')
         );
     }
 }
