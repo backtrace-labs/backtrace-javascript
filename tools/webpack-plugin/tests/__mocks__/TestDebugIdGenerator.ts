59b5590f5a40ae0da5d5805f39bb53b6a4f365c1
Sebastian Alex <sebapotok@gmail.com>
2023-06-22 15:39:16 +0200

    Adds Webpack plugin for injecting debug IDs into sources and sourcemaps. (#9)
    * webpack-plugin: add initial project
    
    * webpack-plugin: add simple source injection and tests
    
    * webpack-plugin: add debugId comment appending to source files
    
    * webpack-plugin: add debugId appending to sourcemap files
    
    * webpack-plugin: use DebugIdGenerator as generator for code
    
    * webpack-plugin: add mode unit tests, add TestDebugIdGenerator
    
    * webpack-plugin: add e2e test builder for standardized tests
    
    * webpack-plugin: fix adding debug IDs to sourcemaps on development and none webpack modes
    
    * webpack-plugin: add additional e2e test cases
    
    * webpack-plugin: move e2e tests to a separate directory
    
    * webpack-plugin: add BacktraceWebpackSourceGenerator and unit tests for it
    
    * webpack-plugin: use ContentAppender in BacktraceWebpackSourceGenerator
    
    * backtrace-javascript: updates package-lock.json
    
    * backtrace-javascript: add webpack-plugin to tsconfig references
    
    * webpack-plugin: add newline before comment in source
    
    * webpack-plugin: add typescript sourcemap e2e test case
    
    * webpack-plugin: add tags to package.json
    
    * webpack-plugin: adds newline before source snippet
    
    * webpack-plugin: add source eval test
    
    * webpack-plugin: add debugID for JS files only
    
    * webpack plugin: add engines to package.json
    
    ---------
    
    Co-authored-by: Sebastian Alex <sebastian.alex@saucelabs.com>

---
+++ tools/webpack-plugin/tests/__mocks__/TestDebugIdGenerator.ts
@@ -0,0 +1,35 @@
+import { DebugIdGenerator, SOURCEMAP_DEBUG_ID_KEY } from '@backtrace/sourcemap-tools';
+
+export class TestDebugIdGenerator implements DebugIdGenerator {
+    public generateSourceSnippet(): string {
+        return `console.log("Source snippet");`;
+    }
+
+    public generateSourceComment(): string {
+        return `//# Source comment`;
+    }
+
+    public addSourceMapKey<T extends object>(sourceMap: T): T & { [SOURCEMAP_DEBUG_ID_KEY]: string } {
+        return {
+            ...sourceMap,
+            [SOURCEMAP_DEBUG_ID_KEY]: 'Source map key',
+        };
+    }
+
+    public static testForSourceSnippet(content: string) {
+        expect(content).toContain(new TestDebugIdGenerator().generateSourceSnippet());
+    }
+
+    public static testForSourceComment(content: string) {
+        expect(content).toContain(new TestDebugIdGenerator().generateSourceComment());
+    }
+
+    public static testForSourceMapKey(sourceMap: object | string) {
+        if (typeof sourceMap === 'string') {
+            sourceMap = JSON.parse(sourceMap);
+        }
+
+        const expected = new TestDebugIdGenerator().addSourceMapKey({});
+        expect(sourceMap).toMatchObject(expected);
+    }
+}
---
+++ tools/webpack-plugin/tests/e2e/helpers.ts
@@ -0,0 +1,96 @@
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack';
+import { BacktracePlugin } from '../../src';
+import { BacktracePluginOptions } from '../../src/BacktracePlugin';
+import { TestDebugIdGenerator } from '../__mocks__/TestDebugIdGenerator';
+
+export interface BaseConfigOptions {
+    tsconfigPath?: string;
+    pluginOptions?: BacktracePluginOptions;
+}
+
+/**
+ * Returns a config with base configuration.
+ *
+ * Input and output should be provided in the arguments.
+ */
+export function getBaseConfig(config: webpack.Configuration, options?: BaseConfigOptions): webpack.Configuration {
+    return {
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: options?.tsconfigPath,
+                    },
+                },
+            ],
+        },
+        plugins: [new BacktracePlugin(options?.pluginOptions ?? { debugIdGenerator: new TestDebugIdGenerator() })],
+        ...config,
+    };
+}
+
+export function webpackModeTest(callback: (testCase: webpack.Configuration['mode']) => void) {
+    const cases: webpack.Configuration['mode'][] = [undefined, 'none', 'development', 'production'];
+
+    for (const testCase of cases) {
+        describe(`Webpack mode: ${testCase}`, () => callback(testCase));
+    }
+}
+
+export function asyncWebpack(config: webpack.Configuration): Promise<webpack.Stats | undefined> {
+    return new Promise((resolve, reject) => {
+        webpack(config, (err, stats) => {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(stats);
+        });
+    });
+}
+
+export function expectSuccess(stats?: webpack.Stats): asserts stats is webpack.Stats {
+    expect(stats).toBeDefined();
+    if (stats?.hasErrors()) {
+        throw new Error(stats.toString());
+    }
+}
+
+export async function expectSourceSnippet(content: string) {
+    TestDebugIdGenerator.testForSourceSnippet(content);
+}
+
+export async function expectSourceComment(content: string) {
+    TestDebugIdGenerator.testForSourceComment(content);
+}
+
+export async function expectSourceMapSnippet(content: string) {
+    TestDebugIdGenerator.testForSourceMapKey(content);
+}
+
+export async function getFiles(dir: string, test?: RegExp) {
+    const files = (await fs.promises.readdir(dir)).map((f) => path.join(dir, f));
+    if (test) {
+        return files.filter((f) => test.test(f));
+    }
+    return files;
+}
+
+export async function removeDir(dir: string) {
+    try {
+        await fs.promises.rm(dir, { recursive: true, force: true });
+    } catch (err) {
+        if ((err as NodeJS.ErrnoException).code === 'ENOENT') {
+            return;
+        }
+
+        throw err;
+    }
+}
