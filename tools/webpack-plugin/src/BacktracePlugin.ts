a806f3b6e2ea10cbe54bafc593750b9e1bf4f177
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-06-22 13:44:59 +0000

    webpack plugin: uses version variable while selecting the plugin

--- tools/webpack-plugin/src/BacktracePlugin.ts
+++ tools/webpack-plugin/src/BacktracePlugin.ts
@@ -5,7 +5,7 @@ import { BacktracePluginV5 } from './BacktracePluginV5';
 let BacktracePlugin: typeof BacktracePluginV4 | typeof BacktracePluginV5;
 
 const version = process.env.WEBPACK_VERSION ?? webpack.version[0];
-switch (process.env.WEBPACK_VERSION?.[0] ?? webpack.version[0]) {
+switch (version) {
     case '4':
         BacktracePlugin = BacktracePluginV4;
         break;
