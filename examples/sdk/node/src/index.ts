a3ee200a251d55695a4d3333dd47d537921f2d65
Konrad Dysput <konrad.dysput@gmail.com>
2023-06-27 16:43:34 +0200

    Node.js example (#27)
    * Node.js example

---
+++ examples/sdk/node/src/index.ts
@@ -0,0 +1,72 @@
+import { BacktraceClient } from '@backtrace/node';
+import fs from 'fs';
+import path from 'path';
+import { exit } from 'process';
+import readline from 'readline';
+import { SUBMISSION_URL } from './consts';
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const client = BacktraceClient.builder({
+    url: SUBMISSION_URL,
+    attachments: [path.join(path.dirname(process.cwd()), 'samplefile.txt')],
+    rateLimit: 5,
+    userAttributes: {
+        'custom-attribute': 'test',
+        'custom-annotation': {
+            prop1: true,
+            prop2: 123,
+        },
+    },
+}).build();
+
+console.log('Welcome to the @Backtrace/node demo');
+
+async function sendHandledException(attributes: Record<string, number>) {
+    console.log('Sending an error to Backtrace');
+    try {
+        fs.readFileSync('/path/to/not/existing/file');
+    } catch (err) {
+        await client.send(err as Error, attributes);
+    }
+}
+
+async function sendMessage(message: string, attributes: Record<string, number>) {
+    console.log('Sending a text message to Backtrace');
+    await client.send(message, attributes);
+}
+
+function showMenu() {
+    const menu =
+        `Please pick one of available options:` +
+        `1. Send an exception` +
+        `2. Send a message` +
+        `0. Exit` +
+        `Type the option number:`;
+    reader.question(menu, async function executeUserOption(optionString: string) {
+        const option = parseInt(optionString);
+
+        const attributes = { selectedOption: option };
+
+        switch (option) {
+            case 1: {
+                return await sendHandledException(attributes);
+            }
+            case 2: {
+                return await sendMessage('test message', attributes);
+            }
+            case 0: {
+                reader.close();
+                return exit(0);
+            }
+            default: {
+                console.log('Selected invalid option. Please try again.');
+            }
+        }
+        return showMenu();
+    });
+}
+
+showMenu();
