0425ccf5e1882d58b1c381297aa9599ab21690a6
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-07-05 10:14:37 +0000

    sourcemap tools: add Logger definition

--- tools/sourcemap-tools/src/index.ts
+++ tools/sourcemap-tools/src/index.ts
@@ -1,6 +1,7 @@
 export * from './ContentAppender';
 export * from './DebugIdGenerator';
 export * from './FileFinder';
+export * from './Logger';
 export * from './SourceProcessor';
 export * from './SymbolUploader';
 export * from './ZipArchive';
