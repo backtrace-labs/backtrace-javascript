ac3df9cf4528960fa782befe81dc4879564585c6
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-07-10 13:21:57 +0000

    cli: add common functions

---
+++ tools/cli/src/helpers/find.ts
@@ -0,0 +1,35 @@
+import { Err, FileFinder, Ok, ResultPromise } from '@backtrace/sourcemap-tools';
+import fs from 'fs';
+import path from 'path';
+
+export async function find(regex: RegExp, ...paths: string[]): ResultPromise<string[], string> {
+    const finder = new FileFinder();
+    const results = new Map<string, string>();
+    for (const findPath of paths) {
+        const stat = await fs.promises.stat(findPath);
+        if (!stat.isDirectory()) {
+            if (!findPath.match(regex)) {
+                return Err(`${findPath} does not match regex: ${regex}`);
+            }
+            const fullPath = path.resolve(findPath);
+            if (!results.has(fullPath)) {
+                results.set(fullPath, findPath);
+            }
+            continue;
+        }
+
+        const findResult = await finder.find(findPath, { match: regex, recursive: true });
+        if (findResult.isErr()) {
+            return findResult;
+        }
+
+        for (const result of findResult.data) {
+            const fullPath = path.resolve(result);
+            if (!results.has(fullPath)) {
+                results.set(fullPath, result);
+            }
+        }
+    }
+
+    return Ok([...results.values()]);
+}
