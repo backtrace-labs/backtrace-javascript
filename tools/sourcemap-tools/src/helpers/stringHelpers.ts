0005ae62a510ead05374cb31de9358f000642972
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-07-18 14:51:09 +0000

    sourcemap tools: add doc to appendBeforeWhitespaces

--- tools/sourcemap-tools/src/helpers/stringHelpers.ts
+++ tools/sourcemap-tools/src/helpers/stringHelpers.ts
@@ -1,3 +1,12 @@
+/**
+ * Appends `value` to `str` before trailing whitespaces in `str`.
+ * @param str String to append to.
+ * @param value String to append.
+ * @example
+ * const str = 'abc\n\n';
+ * const value = 'def';
+ * const appended = appendBeforeWhitespaces(str, value);
+ */
 export function appendBeforeWhitespaces(str: string, value: string) {
     const whitespaces = str.match(/\s*$/)?.[0];
     if (!whitespaces) {
