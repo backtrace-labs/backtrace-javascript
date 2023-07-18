2a9ef47a1949602516b6843026772d6de84e99d1
Konrad Dysput <konrad.dysput@gmail.com>
2023-07-18 12:40:36 +0200

    Node unhandled exceptions/rejections

--- packages/node/src/BacktraceClient.ts
+++ packages/node/src/BacktraceClient.ts
@@ -1,14 +1,16 @@
 import {
     BacktraceAttributeProvider,
-    BacktraceCoreClient,
-    BacktraceRequestHandler,
     BacktraceConfiguration as CoreConfiguration,
+    BacktraceCoreClient,
+    BacktraceReport,
+    BacktraceRequestHandler,
     DebugIdContainer,
     VariableDebugIdMapProvider,
 } from '@backtrace/sdk-core';
-import { BacktraceConfiguration } from './BacktraceConfiguration';
 import { AGENT } from './agentDefinition';
+import { BacktraceConfiguration } from './BacktraceConfiguration';
 import { BacktraceClientBuilder } from './builder/BacktraceClientBuilder';
+import { StrictModeDetector } from './common/StrictModeDetector';
 
 export class BacktraceClient extends BacktraceCoreClient {
     constructor(
@@ -25,9 +27,44 @@ export class BacktraceClient extends BacktraceCoreClient {
             undefined,
             new VariableDebugIdMapProvider(global as DebugIdContainer),
         );
+        this.captureUnhandledErrors();
     }
 
     public static builder(options: BacktraceConfiguration): BacktraceClientBuilder {
         return new BacktraceClientBuilder(options);
+    }
+
+    private captureUnhandledErrors() {
+        const isStrictModeEnabled = StrictModeDetector.enabled();
+
+        process.prependListener(
+            'uncaughtExceptionMonitor',
+            async (error: Error, origin?: 'uncaughtException' | 'unhandledRejection') => {
+                // all rejected promises will be captured via different handler
+                if (origin === 'unhandledRejection') {
+                    return;
+                }
+                await this.send(new BacktraceReport(error, { 'error.type': 'Unhandled exception' }));
+            },
+        );
+
+        process.prependListener('unhandledRejection', async (reason) => {
+            const error =
+                reason instanceof Error
+                    ? reason
+                    : typeof reason === 'string'
+                    ? new Error(reason)
+                    : new Error('Unhandled rejection');
+            await this.send(
+                new BacktraceReport(error, {
+                    'error.type': 'Unhandled exception',
+                }),
+            );
+
+            if (isStrictModeEnabled) {
+                console.error(reason);
+                process.exit(1);
+            }
+        });
     }
 }
---
+++ packages/node/src/common/StrictModeDetector.ts
@@ -0,0 +1,5 @@
+export class StrictModeDetector {
+    public static enabled() {
+        return !this;
+    }
+}
