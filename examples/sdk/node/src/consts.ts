c13a90a0e7d15e9b7e31ed0a374e317a6296fed8
Konrad Dysput <konrad.dysput@gmail.com>
2023-07-18 12:43:28 +0200

    Correct submission URL

--- examples/sdk/node/src/consts.ts
+++ examples/sdk/node/src/consts.ts
@@ -1,2 +1,2 @@
 export const SUBMISSION_URL =
-    'https://kdysput.in.backtrace.io:6098/post?format=json&token=590d39eb154cff1d30f2b689f9a928bb592b25e7e7c10192fe208485ea68d91c';
+    'https://submit.backtrace.io/your-universe/0000000000000000000000000000000000000000000000000000000000000000/json';
