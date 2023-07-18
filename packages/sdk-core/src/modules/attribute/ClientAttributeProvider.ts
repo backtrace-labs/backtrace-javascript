0e28c6b78d8483fc6c9c5c50d65506d655bc62b1
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-07-18 08:55:15 +0000

    sdk: add 'backtrace.agent' attribute specifying submitting agent name

--- packages/sdk-core/src/modules/attribute/ClientAttributeProvider.ts
+++ packages/sdk-core/src/modules/attribute/ClientAttributeProvider.ts
@@ -2,6 +2,7 @@ import { BacktraceAttributeProvider } from './BacktraceAttributeProvider';
 
 export class ClientAttributeProvider implements BacktraceAttributeProvider {
     constructor(
+        private readonly _sdkName: string,
         private readonly _sdkVersion: string,
         private readonly _sessionId: string,
         private readonly _userAttributes: Record<string, unknown>,
@@ -12,6 +13,7 @@ export class ClientAttributeProvider implements BacktraceAttributeProvider {
     public get(): Record<string, unknown> {
         return {
             'application.session': this._sessionId,
+            'backtrace.agent': this._sdkName,
             'backtrace.version': this._sdkVersion,
             ...this._userAttributes,
         };
