8080c13a0555d0deec3a4b554fd2c81419c1df77
Adam Cronin <acronin@backtrace.io>
2023-08-01 16:19:41 -0400

    Moving the React 16 component stack helpers to ReactStackTraceConverter

--- packages/react/src/converters/ReactStackTraceConverter.ts
+++ packages/react/src/converters/ReactStackTraceConverter.ts
@@ -1,7 +1,6 @@
 import { BacktraceStackTraceConverter } from '@backtrace/sdk-core';
 import { JavaScriptEngine } from '@backtrace/sdk-core/src/model/data/JavaScriptEngine';
 import { BacktraceStackFrame } from '@backtrace/sdk-core/lib/model/data/BacktraceStackTrace';
-import { isReact16ComponentStack, parseReact16ComponentStack } from '../helpers/componentStackHelpers';
 
 export class ReactStackTraceConverter implements BacktraceStackTraceConverter {
     constructor(private readonly stackTraceConverter: BacktraceStackTraceConverter) {}
@@ -12,9 +11,41 @@ export class ReactStackTraceConverter implements BacktraceStackTraceConverter {
 
     public convert(stackTrace: string, message = ''): BacktraceStackFrame[] {
         // React 16 component stacks are not JS error stacks, and need to be parsed separately
-        if (isReact16ComponentStack(stackTrace)) {
-            return parseReact16ComponentStack(stackTrace);
+        if (this.isReact16ComponentStack(stackTrace)) {
+            return this.parseReact16ComponentStack(stackTrace);
         }
         return this.stackTraceConverter.convert(stackTrace, message);
+    }
+
+    /** React 16 component stacks need to be parsed separately. React 17+ component stacks can be parsed like Error stacks */
+    private isReact16ComponentStack(stack: string): boolean {
+        if (!stack) {
+            return false;
+        }
+        const frames = stack.split('\n').filter((line) => !!line);
+        for (const frame of frames) {
+            if (!frame.includes('in ')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private parseReact16ComponentStack(stack: string): BacktraceStackFrame[] {
+        const btFrames: BacktraceStackFrame[] = [];
+        if (!stack) {
+            return btFrames;
+        }
+        const frames = stack.split('\n');
+        for (const frame of frames) {
+            if (!frame.trim()) continue;
+            const component = frame.split('in ')[1]?.trim();
+            const btFrame: BacktraceStackFrame = {
+                funcName: component ?? 'unknown',
+                library: 'unknown',
+            };
+            btFrames.push(btFrame);
+        }
+        return btFrames;
     }
 }
