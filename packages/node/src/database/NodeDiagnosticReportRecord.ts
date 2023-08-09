63639b27f2f8680137ac3f247fd2a0e15e8ee353
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-08-09 13:39:42 +0000

    node: add more information to node diagnostic report

--- packages/node/src/database/NodeDiagnosticReportRecord.ts
+++ packages/node/src/database/NodeDiagnosticReportRecord.ts
@@ -35,19 +35,31 @@ export class NodeDiagnosticReportRecord implements BacktraceDatabaseRecord {
         dataBuilder: BacktraceDataBuilder,
     ): NodeDiagnosticReportRecord | undefined {
         try {
-            const record = JSON.parse(json) as NodeDiagnosticReport;
+            const report = JSON.parse(json) as NodeDiagnosticReport;
 
-            const jsStack = record.javascriptStack.stack;
+            const jsStack = report.javascriptStack.stack;
             const validJsStack = jsStack && jsStack[0] !== 'Unavailable.';
 
-            const message = validJsStack ? record.javascriptStack.message : record.header.event;
-            const report = new BacktraceReport(message);
+            const message = validJsStack ? report.javascriptStack.message : report.header.event;
+            const btReport = new BacktraceReport(message, {
+                timestamp: parseInt(report.header.dumpEventTimeStamp),
+                hostname: report.header.host,
+                classifiers: [report.header.trigger],
+                ...this.getUnameData(report),
+                ...this.getCpuData(report),
+                ...this.getMemoryData(report),
+
+                // Annotations
+                'Environment Variables': report.environmentVariables,
+                'Exec Arguments': report.header.commandLine,
+                Error: report,
+            });
 
             if (validJsStack) {
-                report.addStackTrace('main', jsStack.join('\n'));
+                btReport.addStackTrace('main', jsStack.join('\n'));
             }
 
-            const nativeStack = record.nativeStack;
+            const nativeStack = report.nativeStack;
             if (nativeStack) {
                 const nativeFrames = nativeStack.map<BacktraceStackFrame>((frame) => ({
                     funcName: frame.symbol,
@@ -56,18 +68,14 @@ export class NodeDiagnosticReportRecord implements BacktraceDatabaseRecord {
 
                 // If the JS stack is valid, add as 'native', otherwise 'main'
                 const threadName = validJsStack ? 'native' : 'main';
-                report.addStackTrace(threadName, nativeFrames);
+                btReport.addStackTrace(threadName, nativeFrames);
             }
 
-            const isOom = record.header.event === 'Allocation failed - JavaScript heap out of memory';
-            if (isOom) {
-                const errorType: BacktraceErrorType = 'OOMException';
-                report.attributes['error.type'] = errorType;
-            }
+            const isOom = report.header.event === 'Allocation failed - JavaScript heap out of memory';
+            const errorType: BacktraceErrorType = isOom ? 'OOMException' : 'Exception';
+            btReport.attributes['error.type'] = errorType;
 
-            // TODO: Add more attributes
-
-            const data = dataBuilder.build(report);
+            const data = dataBuilder.build(btReport);
 
             return {
                 id,
@@ -80,5 +88,37 @@ export class NodeDiagnosticReportRecord implements BacktraceDatabaseRecord {
         } catch {
             return undefined;
         }
+    }
+
+    private static getUnameData(report: NodeDiagnosticReport) {
+        return {
+            'uname.sysname': report.header.osName,
+            'uname.release': report.header.osRelease,
+            'uname.version': report.header.osVersion,
+            'uname.machine': report.header.osMachine,
+        };
+    }
+
+    private static getCpuData(report: NodeDiagnosticReport) {
+        const cpu = report.header.cpus[0];
+        return {
+            'cpu.arch': report.header.arch,
+            'cpu.brand': cpu.model,
+            'cpu.frequency': cpu.speed,
+            'cpu.user': cpu.user,
+            'cpu.nice': cpu.nice,
+            'cpu.sys': cpu.sys,
+            'cpu.idle': cpu.idle,
+            'cpu.irq': cpu.irq,
+            'cpu.count': report.header.cpus.length,
+        };
+    }
+
+    private static getMemoryData(report: NodeDiagnosticReport) {
+        return {
+            'vm.rss.size': Math.round(report.javascriptHeap.usedMemory / 1024),
+            'vm.rss.peak': Math.round(report.javascriptHeap.totalMemory / 1024),
+            'vm.rss.available': report.javascriptHeap.availableMemory,
+        };
     }
 }
