3526d891913e50f3dcde327e5039f3e4e9913ae0
adamcronin42 <30831164+adamcronin42@users.noreply.github.com>
2023-08-14 13:43:57 -0400

    Properly making @backtrace/browser a dependency (#72)
    * Properly making @backtrace/browser and @backtrace/sdk-core dependencies
    
    * Removing dependency on @backtrace/sdk-core
    
    ---------
    
    Co-authored-by: Adam Cronin <acronin@backtrace.io>

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,4 +1,6 @@
-export { BacktraceReport } from '@backtrace/sdk-core';
+export { BacktraceReport, BacktraceStackTraceConverter } from '@backtrace/sdk-core';
+export { JavaScriptEngine } from '@backtrace/sdk-core/lib/model/data/JavaScriptEngine';
+export { BacktraceStackFrame } from '@backtrace/sdk-core/lib/model/data/BacktraceStackTrace';
 export * from '@backtrace/sdk-core/lib/model/attachment';
 export * from './BacktraceClient';
 export * from './BacktraceConfiguration';
