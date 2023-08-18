16ec60323168479d01bbf3e19d1892d033e79454
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-08-18 14:06:41 +0000

    react-native: externalize react packages

--- packages/react-native/webpack.config.js
+++ packages/react-native/webpack.config.js
@@ -10,6 +10,10 @@ const common = {
     devtool: 'source-map',
     entry: './src/index.ts',
     plugins: [agentDefinitionPlugin(path.join(__dirname, 'package.json'))],
+    externals: {
+        'react': true,
+        'react-native': true
+    }
 };
 
 /** @type {Array<import('webpack').Configuration>} */
