a268cf0d01e90dffd493b15c42ad8801095b5d4b
Konrad Dysput <konrad.dysput@gmail.com>
2023-08-24 21:06:39 +0200

    Detect missing information about the agent and fallback to the default (#88)

--- packages/browser/src/engineDetector.ts
+++ packages/browser/src/engineDetector.ts
@@ -1,6 +1,9 @@
 import { JavaScriptEngine } from '@backtrace/sdk-core/lib/model/data/JavaScriptEngine';
 
 export function getEngine(): JavaScriptEngine {
+    if (!navigator.userAgent) {
+        return 'v8';
+    }
     const normalizedUserAgent = navigator.userAgent.toLowerCase();
 
     if (normalizedUserAgent.includes('firefox')) {
