b89a0d0d1027a54e28a5fa1540e42ffd3a12f1bc
Konrad Dysput <konrad.dysput@gmail.com>
2023-08-29 17:00:38 +0200

    React native api improvements

--- packages/react-native/src/BacktraceClient.ts
+++ packages/react-native/src/BacktraceClient.ts
@@ -1,8 +1,44 @@
-import { BacktraceClient as BacktraceReactClient } from '@backtrace-labs/react';
+import { ReactStackTraceConverter } from '@backtrace-labs/react';
+import {
+    BacktraceCoreClient,
+    SingleSessionProvider,
+    V8StackTraceConverter,
+    VariableDebugIdMapProvider,
+    type BacktraceAttributeProvider,
+    type BacktraceRequestHandler,
+    type BreadcrumbsEventSubscriber,
+    type DebugIdContainer,
+} from '@backtrace-labs/sdk-core';
 import { BacktraceClientBuilder } from './BacktraceClientBuilder';
 import { type BacktraceConfiguration } from './BacktraceConfiguration';
 
-export class BacktraceClient extends BacktraceReactClient {
+export class BacktraceClient extends BacktraceCoreClient {
+    private static _instance?: BacktraceClient;
+    constructor(
+        options: BacktraceConfiguration,
+        requestHandler: BacktraceRequestHandler,
+        attributeProviders: BacktraceAttributeProvider[],
+        breadcrumbsEventSubscribers: BreadcrumbsEventSubscriber[],
+    ) {
+        super({
+            options,
+            sdkOptions: {
+                agent: '@backtrace/react',
+                agentVersion: '1.0.0',
+                langName: 'react-native',
+                langVersion: 'unknown',
+            },
+            requestHandler,
+            attributeProviders,
+            debugIdMapProvider: new VariableDebugIdMapProvider(global as DebugIdContainer),
+            breadcrumbsSetup: {
+                subscribers: breadcrumbsEventSubscribers,
+            },
+            stackTraceConverter: new ReactStackTraceConverter(new V8StackTraceConverter()),
+            sessionProvider: new SingleSessionProvider(),
+        });
+    }
+
     public static builder(options: BacktraceConfiguration): BacktraceClientBuilder {
         return new BacktraceClientBuilder(options);
     }
