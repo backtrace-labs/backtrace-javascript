6e4bb4c98133b3b67013a94f14bda6e8097b79b0
Konrad Dysput <konrad.dysput@gmail.com>
2023-08-29 17:02:16 +0200

    Browser - simplify the Form data usage, export BacktraceBrowserRequestHandler, fix the issue with ignoring jsonEscape in the formatter function

--- packages/browser/src/BacktraceBrowserRequestHandler.ts
+++ packages/browser/src/BacktraceBrowserRequestHandler.ts
@@ -42,7 +42,12 @@ export class BacktraceBrowserRequestHandler implements BacktraceRequestHandler {
             const response = await fetch(submissionUrl, {
                 method: 'POST',
                 body: payload,
-                headers: typeof payload === 'string' ? this.JSON_HEADERS : {},
+                headers:
+                    typeof payload === 'string'
+                        ? this.JSON_HEADERS
+                        : {
+                              'Content-Type': 'multipart/form-data',
+                          },
                 signal: controller.signal,
             });
 
@@ -82,8 +87,7 @@ export class BacktraceBrowserRequestHandler implements BacktraceRequestHandler {
 
     private createFormData(json: string, attachments: BacktraceAttachment<Blob | string>[]) {
         const formData = new FormData();
-        const blob = new Blob([json]);
-        formData.append(this.UPLOAD_FILE_NAME, blob, `${this.UPLOAD_FILE_NAME}.json`);
+        formData.append(this.UPLOAD_FILE_NAME, json);
 
         if (!attachments || attachments.length === 0) {
             return formData;
