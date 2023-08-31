abb6ba1a5e15593603dae686e602fd2b1ca25564
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-08-31 13:45:20 +0000

    cli: cache loaded options in memory

--- tools/cli/src/options/loadOptions.ts
+++ tools/cli/src/options/loadOptions.ts
@@ -5,11 +5,17 @@ import { CliOptions, CommandCliOptions } from './models/CliOptions';
 export const DEFAULT_OPTIONS_PATH = '.backtracejsrc';
 
 export function loadAndJoinOptions(path?: string) {
+    let readOptions: CliOptions | undefined;
+
     return async function loadAndJoinOptions<K extends keyof CommandCliOptions>(
         key: K,
         options: Partial<CommandCliOptions[K]>,
         defaults?: Partial<CommandCliOptions[K]>,
     ): ResultPromise<Partial<CommandCliOptions[K]>, string> {
+        if (readOptions) {
+            return Ok(joinOptions(key, options, defaults)(readOptions));
+        }
+
         const readResult = await readFile(path ?? DEFAULT_OPTIONS_PATH);
         if (readResult.isErr()) {
             return path ? readResult : Ok(options);
@@ -17,6 +23,7 @@ export function loadAndJoinOptions(path?: string) {
 
         return AsyncResult.equip(readResult)
             .then(parseJSONC<CliOptions>)
+            .then((opts) => (readOptions = opts))
             .then(joinOptions(key, options, defaults)).inner;
     };
 }
