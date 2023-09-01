3bf991883ed93133264c8eeb09f77e63249f8f5f
Konrad Dysput <konrad.dysput@gmail.com>
2023-09-01 14:02:04 +0200

    Basic attributes

--- packages/react-native/src/BacktraceClientBuilder.ts
+++ packages/react-native/src/BacktraceClientBuilder.ts
@@ -1,11 +1,18 @@
 import { BacktraceBrowserRequestHandler } from '@backtrace-labs/react';
 import { BacktraceCoreClientBuilder, SingleSessionProvider } from '@backtrace-labs/sdk-core';
+import { ApplicationInformationAttributeProvider } from './attributes/ApplicationInformationAttributeProvider';
+import { DeviceAttributeProvider } from './attributes/DeviceAttributeProvider';
 import { BacktraceClient } from './BacktraceClient';
 import { type BacktraceConfiguration } from './BacktraceConfiguration';
 
 export class BacktraceClientBuilder extends BacktraceCoreClientBuilder<BacktraceClient> {
     constructor(private readonly options: BacktraceConfiguration) {
-        super(new BacktraceBrowserRequestHandler(options), [], [], new SingleSessionProvider());
+        super(
+            new BacktraceBrowserRequestHandler(options),
+            [new ApplicationInformationAttributeProvider(), new DeviceAttributeProvider()],
+            [],
+            new SingleSessionProvider(),
+        );
     }
     public build(): BacktraceClient {
         return new BacktraceClient(this.options, this.handler, this.attributeProviders, this.breadcrumbsSubscribers);
