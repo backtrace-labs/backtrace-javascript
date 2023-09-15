3281527a7e626a6fee84ea4630d8ffbeef31d14f
Konrad Dysput <konrad.dysput@gmail.com>
2023-09-15 18:28:34 +0200

    Expose parsers from react client

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,5 @@
 export * from '@backtrace-labs/browser';
 export { BacktraceClient } from './BacktraceClient';
+export * from './builder/BacktraceReactClientBuilder';
+export * from './converters/ReactStackTraceConverter';
 export * from './ErrorBoundary';
