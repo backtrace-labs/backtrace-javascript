df7b69597ea9de563914a94d774af1815def8826
Konrad Dysput <konrad.dysput@gmail.com>
2023-09-29 12:59:39 +0200

    Re-indent file

--- packages/react-native/ios/BacktraceApplicationAttributeProvider.mm
+++ packages/react-native/ios/BacktraceApplicationAttributeProvider.mm
@@ -1,14 +1,14 @@
 #import "BacktraceApplicationAttributeProvider.h"
 
 @implementation BacktraceApplicationAttributeProvider
-    RCT_EXPORT_MODULE()
-    RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(get) {
-        NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-        NSString *displayName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
-        NSString *bundleName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
-        [dictionary setObject: displayName ? displayName : bundleName forKey: @"application"];
-        [dictionary setObject: [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] ? displayName : bundleName forKey: @"application.version"];
-        return dictionary;
-    }
+RCT_EXPORT_MODULE()
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(get) {
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    NSString *displayName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+    NSString *bundleName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
+    [dictionary setObject: displayName ? displayName : bundleName forKey: @"application"];
+    [dictionary setObject: [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] ? displayName : bundleName forKey: @"application.version"];
+    return dictionary;
+}
 
 @end
