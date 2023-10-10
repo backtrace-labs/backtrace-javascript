74fa304c924be59ff22aa248cabdd8990dd1cd78
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-10-10 15:17:30 +0200

    react-native: bring back missing attribute providers

--- packages/react-native/src/builder/BacktraceClientBuilder.ts
+++ packages/react-native/src/builder/BacktraceClientBuilder.ts
@@ -2,29 +2,36 @@ import { BacktraceCoreClientBuilder } from '@backtrace-labs/sdk-core';
 import { Platform } from 'react-native';
 import { BacktraceClient } from '../BacktraceClient';
 import { NativeAttributeProvider } from '../attributes/NativeAttributeProvider';
+import { ReactNativeAttributeProvider } from '../attributes/ReactNativeAttributeProvider';
+import { DebuggerHelper } from '../common/DebuggerHelper';
 import type { BacktraceClientSetup } from './BacktraceClientSetup';
 
 export class BacktraceClientBuilder extends BacktraceCoreClientBuilder<BacktraceClientSetup> {
     constructor(clientSetup: BacktraceClientSetup) {
         super(clientSetup);
 
-        const attributeProviders = Platform.select({
-            ios: [
-                new NativeAttributeProvider('BacktraceApplicationAttributeProvider', 'scoped'),
-                new NativeAttributeProvider('BacktraceDeviceAttributeProvider', 'scoped'),
-                new NativeAttributeProvider('BacktraceSystemAttributeProvider', 'scoped'),
-                new NativeAttributeProvider('BacktraceMemoryUsageAttributeProvider', 'dynamic'),
-                new NativeAttributeProvider('BacktraceCpuAttributeProvider', 'dynamic'),
-            ],
-            android: [
-                new NativeAttributeProvider('BacktraceApplicationAttributeProvider', 'scoped'),
-                new NativeAttributeProvider('BacktraceDeviceAttributeProvider', 'scoped'),
-                new NativeAttributeProvider('BacktraceSystemAttributeProvider', 'scoped'),
-                new NativeAttributeProvider('MemoryInformationAttributeProvider', 'dynamic'),
-                new NativeAttributeProvider('ProcessAttributeProvider', 'dynamic'),
-            ],
-            default: [],
-        });
+        const attributeProviders = [
+            new ReactNativeAttributeProvider(),
+            ...(DebuggerHelper.isConnected()
+                ? []
+                : Platform.select({
+                      ios: [
+                          new NativeAttributeProvider('BacktraceApplicationAttributeProvider', 'scoped'),
+                          new NativeAttributeProvider('BacktraceDeviceAttributeProvider', 'scoped'),
+                          new NativeAttributeProvider('BacktraceSystemAttributeProvider', 'scoped'),
+                          new NativeAttributeProvider('BacktraceMemoryUsageAttributeProvider', 'dynamic'),
+                          new NativeAttributeProvider('BacktraceCpuAttributeProvider', 'dynamic'),
+                      ],
+                      android: [
+                          new NativeAttributeProvider('BacktraceApplicationAttributeProvider', 'scoped'),
+                          new NativeAttributeProvider('BacktraceDeviceAttributeProvider', 'scoped'),
+                          new NativeAttributeProvider('BacktraceSystemAttributeProvider', 'scoped'),
+                          new NativeAttributeProvider('MemoryInformationAttributeProvider', 'dynamic'),
+                          new NativeAttributeProvider('ProcessAttributeProvider', 'dynamic'),
+                      ],
+                      default: [],
+                  })),
+        ];
 
         for (const provider of attributeProviders) {
             this.addAttributeProvider(provider);
