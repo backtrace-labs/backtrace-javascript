7f74bd2eaeb17241c320aba2543fab216a3c6fe9
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-10-16 17:14:16 +0200

    electron: add app anme and version attributes to main

--- packages/electron/src/main/builder/BacktraceClientBuilder.ts
+++ packages/electron/src/main/builder/BacktraceClientBuilder.ts
@@ -1,7 +1,14 @@
 import { BacktraceNodeClientSetup, BacktraceClientBuilder as NodeBacktraceClientBuilder } from '@backtrace-labs/node';
 import { BacktraceClient } from '../BacktraceClient';
+import { ApplicationInformationAttributeProvider } from '../attributes/ApplicationInformationAttributeProvider';
 
 export class BacktraceClientBuilder extends NodeBacktraceClientBuilder {
+    constructor(clientSetup: BacktraceNodeClientSetup) {
+        super(clientSetup);
+
+        this.addAttributeProvider(new ApplicationInformationAttributeProvider());
+    }
+
     public build(): BacktraceClient {
         const instance = new BacktraceClient(this.clientSetup as BacktraceNodeClientSetup);
         instance.initialize();
