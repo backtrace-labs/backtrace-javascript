5fc74647f9ca7624d2e3d595984eb89443083d62
Sebastian Alex <sebastian.alex@saucelabs.com>
2023-10-19 15:59:09 +0000

    electron example: restore example submission URL

--- examples/sdk/electron/src/main/consts.ts
+++ examples/sdk/electron/src/main/consts.ts
@@ -1,2 +1,2 @@
 export const SUBMISSION_URL =
-    'https://salex.in.backtrace.io:6098/post?format=json&token=81ebe51f9caa641c2013b15b59f704a853a70e542ce279a90bf1526853d8ff0a';
+    'https://submit.backtrace.io/your-universe/0000000000000000000000000000000000000000000000000000000000000000/json';
