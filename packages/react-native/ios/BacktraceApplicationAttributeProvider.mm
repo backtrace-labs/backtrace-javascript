7f06777b1b885bd7a95965ecd4b5ae8f7080fed6
Konrad Dysput <konrad.dysput@gmail.com>
2024-04-26 16:53:56 +0200

    react-native: Fixed application.version attribute

--- packages/react-native/ios/BacktraceApplicationAttributeProvider.mm
+++ packages/react-native/ios/BacktraceApplicationAttributeProvider.mm
@@ -7,7 +7,7 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(get) {
     NSString *displayName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
     NSString *bundleName = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"];
     [dictionary setObject: displayName ? displayName : bundleName forKey: @"application"];
-    [dictionary setObject: [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] ? displayName : bundleName forKey: @"application.version"];
+    [dictionary setObject: [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] forKey: @"application.version"];
     return dictionary;
 }
 
