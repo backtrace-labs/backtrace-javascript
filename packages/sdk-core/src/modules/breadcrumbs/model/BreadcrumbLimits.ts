6c4184162ff86ea172d7d53828872469ee7b056d
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-05-24 15:17:13 +0000

    sdk-core: PR suggestion changes

--- packages/sdk-core/src/modules/breadcrumbs/model/BreadcrumbLimits.ts
+++ packages/sdk-core/src/modules/breadcrumbs/model/BreadcrumbLimits.ts
@@ -1,30 +1,39 @@
 export interface BreadcrumbLimits {
     /**
-     * Specifies maximum number of breadcrumbs stored by the library. By default, only 100 breadcrumbs
-     * will be stored.
+     * Specifies maximum number of breadcrumbs stored by the library.
+     *
+     * @default 100
      */
     readonly maximumBreadcrumbs?: number;
 
     /**
      * Specifies maximum object depth that are included in breadcrumb attributes.
+     *
+     * @default 2
      */
     readonly maximumAttributesDepth?: number;
 
     /**
      * Specifies maximum breadcrumb message length.
      * If the size is exceeded, message will be truncated.
+     *
+     * @default 255
      */
     readonly maximumBreadcrumbMessageLength?: number;
 
     /**
      * Specifies maximum single breadcrumb size in bytes.
      * If the size is exceeded, the breadcrumb will be skipped.
+     *
+     * @default 65536 // 64kB
      */
     readonly maximumBreadcrumbSize?: number;
 
     /**
      * Specifies maximum breadcrumbs size in bytes.
      * If the size is exceeded, oldest breadcrumbs will be skipped.
+     *
+     * @default 1048576 // 1MB
      */
-    readonly maximumBreadcrumbsSize?: number;
+    readonly maximumTotalBreadcrumbsSize?: number;
 }
