926a4bba01541602c1098ea7f5c1fa17cdf69924
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-07-26 15:29:08 +0200

    node: replace createFromSession with getSessionAttachments in FileBreacrumbsStorage, expose it from package

--- packages/node/src/BacktraceClient.ts
+++ packages/node/src/BacktraceClient.ts
@@ -299,10 +299,7 @@ export class BacktraceClient extends BacktraceCoreClient<BacktraceConfiguration>
         for (const [recordPath, report, session] of reports) {
             try {
                 if (session) {
-                    const breadcrumbsStorage = FileBreadcrumbsStorage.createFromSession(session, this.nodeFileSystem);
-                    if (breadcrumbsStorage) {
-                        report.attachments.push(...breadcrumbsStorage.getAttachments());
-                    }
+                    report.attachments.push(...FileBreadcrumbsStorage.getSessionAttachments(session));
 
                     const fileAttributes = FileAttributeManager.createFromSession(session, this.nodeFileSystem);
                     Object.assign(report.attributes, await fileAttributes.get());
--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -14,6 +14,6 @@ export {
 export * from './BacktraceClient';
 export * from './BacktraceConfiguration';
 export * from './BacktraceNodeRequestHandler';
-export * from './attachment';
+export * from './breadcrumbs';
 export * from './builder/BacktraceClientBuilder';
 export * from './builder/BacktraceClientSetup';
