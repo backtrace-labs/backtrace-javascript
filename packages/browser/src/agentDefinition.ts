37b5dec97a6f5ca762f00ed0c99a9ad1a2f42184
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-08-22 13:24:35 +0200

    browser: change output to esm, bundle with rollup

--- packages/browser/src/agentDefinition.ts
+++ packages/browser/src/agentDefinition.ts
@@ -1,12 +1,8 @@
 import type { SdkOptions } from '@backtrace/sdk-core/lib/builder/SdkOptions';
 
-// These variables will be set on compilation stage
-declare const BACKTRACE_AGENT_NAME: string;
-declare const BACKTRACE_AGENT_VERSION: string;
-
 export const AGENT: SdkOptions = {
     langName: 'js',
     langVersion: navigator.userAgent,
-    agent: BACKTRACE_AGENT_NAME,
-    agentVersion: BACKTRACE_AGENT_VERSION,
+    agent: 'BACKTRACE_AGENT_NAME',
+    agentVersion: 'BACKTRACE_AGENT_VERSION',
 };
