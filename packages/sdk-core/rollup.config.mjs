592cb63a9c6294dc16e4eaefaac0e4a40ee5245e
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-08-22 12:35:27 +0200

    sdk-core: change output to esm

---
+++ packages/sdk-core/rollup.config.mjs
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2';
+
+/** @type {import('rollup').RollupOptions} */
+export default {
+    input: './src/index.ts',
+    output: [
+        {
+            file: 'lib/bundle.mjs',
+            format: 'esm',
+            sourcemap: true,
+        },
+        {
+            file: 'lib/bundle.cjs',
+            format: 'cjs',
+            sourcemap: true,
+        },
+    ],
+    external: [/node_modules/],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
+};
