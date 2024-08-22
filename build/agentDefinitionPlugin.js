e545aa2a1ef1cd21773337a47104773a5869daa6
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-08-22 14:45:13 +0200

    build: remove agentDefinitionPlugin

--- build/agentDefinitionPlugin.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = function agentDefinitionPlugin(packageJsonPath) {
-    const packageJson = require(packageJsonPath);
-    return new webpack.DefinePlugin({
-        BACKTRACE_AGENT_NAME: JSON.stringify(packageJson.name),
-        BACKTRACE_AGENT_VERSION: JSON.stringify(packageJson.version),
-    });
-};
