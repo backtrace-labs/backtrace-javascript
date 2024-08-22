90688560f6755c1f1e0dd01ae1a19b0146a33539
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-08-22 13:35:47 +0200

    react: change output to esm, bundle with rollup

--- packages/react/src/agentDefinition.ts
+++ packages/react/src/agentDefinition.ts
@@ -1,12 +1,8 @@
 import type { SdkOptions } from '@backtrace/sdk-core';
 
-// These variables will be set on compilation stage
-declare const BACKTRACE_AGENT_NAME: string;
-declare const BACKTRACE_AGENT_VERSION: string;
-
 export const AGENT: SdkOptions = {
     langName: 'react',
     langVersion: navigator.userAgent,
-    agent: BACKTRACE_AGENT_NAME,
-    agentVersion: BACKTRACE_AGENT_VERSION,
+    agent: 'BACKTRACE_AGENT_NAME',
+    agentVersion: 'BACKTRACE_AGENT_VERSION',
 };
