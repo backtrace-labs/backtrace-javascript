8d2fa639a7e7707a4ce335821a6308c5f4159952
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-09-03 11:26:34 +0200

    chore: fix prettier issues

--- smoketests/tests/__helpers/urls.ts
+++ smoketests/tests/__helpers/urls.ts
@@ -1,4 +1,4 @@
-import assert from "assert";
+import assert from 'assert';
 
 assert(process.env.SMOKETESTS_SUBMIT_LAYER_URL, 'SMOKETESTS_SUBMIT_LAYER_URL is not set');
 assert(process.env.SMOKETESTS_DIRECT_SUBMIT_URL, 'SMOKETESTS_DIRECT_SUBMIT_URL is not set');
