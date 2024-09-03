f4561dc2642efd535bf4ef583f10564a13c40378
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-09-03 11:19:43 +0200

    smoketests: fix npm command on Windows

--- smoketests/tests/__helpers/npm.ts
+++ smoketests/tests/__helpers/npm.ts
@@ -2,7 +2,8 @@ import { SpawnOptionsWithoutStdio } from 'child_process';
 import { asyncSpawn } from './asyncSpawn';
 
 export function npm(cwd: string, args: string[], options?: SpawnOptionsWithoutStdio) {
-    return asyncSpawn('npm', args, {
+    const command = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+    return asyncSpawn(command, args, {
         ...options,
         cwd,
     });
