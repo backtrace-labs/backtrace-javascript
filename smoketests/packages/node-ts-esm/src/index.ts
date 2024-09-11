e049acb4473c68e2c945b6cb6616681393370980
Sebastian Alex <sebastian.alex@saucelabs.com>
2024-09-11 18:20:55 +0200

    smoketests: add more node smoketests with database

--- smoketests/packages/node-ts-esm/src/index.ts
+++ smoketests/packages/node-ts-esm/src/index.ts
@@ -1,9 +1,21 @@
 import { BacktraceClient } from '@backtrace/node';
+import { BacktraceReportSubmissionResult, BacktraceSubmissionResponse } from '@backtrace/sdk-core';
+import fs from 'fs/promises';
 import path from 'path';
 
-const SUBMISSION_URL = process.argv[2];
+const DATABASE_TEST = 'test-database';
+const DATABASE_PATH = path.join(process.cwd(), 'database');
+
+const TIMEOUT = 5000;
+const [SUBMISSION_URL, TEST] = process.argv.slice(2);
 if (!SUBMISSION_URL) {
     throw new Error('submission URL is required');
+}
+
+// If current test is not a database test, clean the database
+// to avoid invalid results
+if (TEST !== DATABASE_TEST) {
+    await fs.rm(DATABASE_PATH, { force: true, recursive: true });
 }
 
 console.log('initializing BacktraceClient');
@@ -19,19 +31,69 @@ const client = BacktraceClient.initialize({
     },
     database: {
         enable: true,
-        path: path.join(process.cwd(), 'database'),
+        path: DATABASE_PATH,
         captureNativeCrashes: true,
         createDatabaseDirectory: true,
     },
 });
 
-console.log('submitting message');
-client.send('test message').then((result) => {
-    if (result.status !== 'Ok') {
+function waitForSend(timeout: number) {
+    return new Promise<BacktraceReportSubmissionResult<BacktraceSubmissionResponse>>((resolve, reject) => {
+        let resolved = false;
+        client.once('after-send', (data, attachments, result) => {
+            if (!resolved) {
+                resolved = true;
+                resolve(result);
+            }
+        });
+
+        setTimeout(() => {
+            if (!resolved) {
+                resolved = true;
+                reject(new Error('Wait for send timed out.'));
+            }
+        }, timeout);
+    });
+}
+
+async function test(fn: () => Promise<BacktraceReportSubmissionResult<BacktraceSubmissionResponse>> | void) {
+    const waitPromise = waitForSend(TIMEOUT);
+    const result = (await fn()) ?? (await waitPromise);
+    if (result.status === 'Ok') {
+        console.log(result.result?._rxid);
+        process.exit(0);
+    } else {
         console.error('Unexpected result:', result);
         process.exit(1);
     }
+}
 
-    console.log(result.result?._rxid);
-    process.exit(0);
-});
+console.log(`running test ${TEST}`);
+switch (TEST ?? 'test-exception') {
+    case DATABASE_TEST: {
+        await test(() => {
+            // do nothing, expect database to send something
+        });
+        break;
+    }
+    case 'test-unhandled-exception':
+        await test(() => {
+            setTimeout(() => {
+                throw new Error('unhandled exception');
+            }, 10);
+        });
+        break;
+    case 'test-unhandled-rejection':
+        await test(() => {
+            new Promise((_, reject) => reject(new Error('unhandled rejection')));
+        });
+        break;
+    case 'test-message':
+        await test(() => client.send('test message'));
+        break;
+    case 'test-exception':
+        await test(() => client.send(new Error('test exception')));
+        break;
+    default:
+        throw new Error(`unknown test ${TEST}`);
+}
--- smoketests/tests/jest/node/ts-esm.spec.ts
+++ smoketests/tests/jest/node/ts-esm.spec.ts
@@ -1,9 +1,12 @@
 import path from 'path';
 import { PACKAGE_DIR } from '../../__helpers/packages.js';
-import { addSubmitTests } from './tests.js';
+import { addCrashAndSubmitTests, addSubmitTests } from './tests.js';
 
 const NODE_APP_PATH = path.join(PACKAGE_DIR, 'node-ts-esm');
 
 describe('node-ts-esm', () => {
-    addSubmitTests(NODE_APP_PATH);
+    addSubmitTests(NODE_APP_PATH, 'test-message');
+    addSubmitTests(NODE_APP_PATH, 'test-exception');
+    addCrashAndSubmitTests(NODE_APP_PATH, 'test-unhandled-exception');
+    addCrashAndSubmitTests(NODE_APP_PATH, 'test-unhandled-rejection');
 });
