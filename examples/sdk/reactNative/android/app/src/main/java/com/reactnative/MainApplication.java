d8680501580bcd3ef1c1ac84fc78d27caad16806
Sebastian Alex <sebapotok@gmail.com>
2024-12-09 18:55:46 +0100

    react-native example: update to react-native 0.75.3 boilerplate code (#292)
    * react-native example: update to newest react-native boilerplate code
    
    * react-native example: change attach to RN app type in launch.json
    
    * react-native example: rename app name to reactNative
    
    * react-native example: remove @backtrace/react dep
    
    * react-native example: restore Android .java classes, remove Kotlin
    
    * react-native example: remove debug test method
    
    * react-native example: remove SuppressWarnings and comments from MainApplication.getPackages
    
    ---------
    
    Co-authored-by: Sebastian Alex <sebastian.alex@saucelabs.com>

--- examples/sdk/reactNative/android/app/src/main/java/com/reactnative/MainApplication.java
+++ examples/sdk/reactNative/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -12,37 +12,34 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          packages.add(new BacktraceDemoPackage());
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      packages.add(new BacktraceDemoPackage());
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
 
-        @Override
-        protected boolean isNewArchEnabled() {
-          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+    @Override
+    protected boolean isNewArchEnabled() {
+      return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
 
-        @Override
-        protected Boolean isHermesEnabled() {
-          return BuildConfig.IS_HERMES_ENABLED;
-        }
-      };
+    @Override
+    protected Boolean isHermesEnabled() {
+      return BuildConfig.IS_HERMES_ENABLED;
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -54,9 +51,9 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      // If you opted-in for the New Architecture, we load the native entry point for
+      // this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
