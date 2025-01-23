964ff07d0f452a1e52f45b9ed1429bb3d842f325
Konrad Dysput <konrad.dysput@saucelabs.com>
2025-01-23 12:47:49 +0100

    react-native: move upload script to the react-native directory and process source map via node, not via jq

---
+++ packages/react-native/addDebugIdToSourceMap.js
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+const fs = require('fs');
+const path = require('path');
+
+function includeDebugIdInSourceMap(sourceMapPath, debugId) {
+    if (!fs.existsSync(sourceMapPath)) {
+        console.error(`Backtrace: source map not found at path ${sourceMapPath}.`);
+        process.exit(1);
+    }
+
+    const sourceMapContent = fs.readFileSync(sourceMapPath, 'utf8');
+    const sourceMap = JSON.parse(sourceMapContent);
+
+    sourceMap['debugId'] = debugId;
+
+    fs.writeFileSync(sourceMapPath, JSON.stringify(sourceMap), 'utf8');
+    console.debug(`Backtrace: Successfully updated source map file ${sourceMapPath} with debug id: ${debugId}`);
+}
+
+const args = process.argv.slice(2);
+if (args.length < 2) {
+    console.error('Usage: node update-json.js <sourcemapPath> <debugId>');
+    process.exit(1);
+}
+
+const sourceMapPath = path.resolve(args[0]);
+const debugId = args[1];
+includeDebugIdInSourceMap(sourceMapPath, debugId);
--- packages/react-native/ios-sourcemap-upload.sh
+++ packages/react-native/ios-sourcemap-upload.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Script responsible for preprocessing source maps with debugid and uploading it to Backtrace via backtrace-js. 
-# Usage:    ./ios-sourcemap-upload.sh <source_map_file_path> <debug_id_file_path> <backtrace_configuration_path>
+# Usage:    ./ios-sourcemap-upload.sh <source_map_file_path> <debug_id_file_path> <backtrace_configuration_path> <project_directory_path>
 # Parameters:
-#   <source_map_file_path>        (Required) Path to the source map file.
-#   <debug_id_file_path>            (Required) path to generated backtrace debug id.
-#   <backtrace_configuration_path>      (Required) Path to the .backtracejsrc configuration file.
+#   <source_map_file_path>          (Required) Path to the source map file.
+#   <debug_id_file_path>            (Required) Path to generated backtrace debug id.
+#   <backtrace_configuration_path>  (Required) Path to the .backtracejsrc configuration file.
+#   <project_directory_path>        (Required) Path to the react-native project directory
 #
 # Adjusting metro configuration is required in order to correctly use debug_id available in the debug_id_file_path.
 
@@ -38,19 +39,27 @@ if [ -z "$3" ]; then
     exit 1
 fi
 
+if [ -z "$4" ]; then
+    echo "Error: Missing path to the project directory."
+    exit 1
+fi
+
+project_directory_path="$4"
+
 backtrace_configuration_path="$3"
-
-debug_id=$(<"$debug_id_file_path")
-
-jq ". += {\"debugId\": \"$debug_id\"}" "$source_map_file_path" > "${source_map_file_path}.tmp" && mv "${source_map_file_path}.tmp" $source_map_file_path
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# path to react-native module dir relative from this script
-backtrace_js_path="${script_dir}/node_modules/.bin/backtrace-js"
 
 # check and assign NODE_BINARY env
 source "$REACT_NATIVE_PATH/scripts/node-binary.sh"
+debug_id=$(<"$debug_id_file_path")
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$NODE_BINARY" "$script_dir/addDebugIdToSourceMap.js" \
+    "$source_map_file_path" \
+    "$debug_id"
+
+# path to react-native module dir relative from this script
+backtrace_js_path="${project_directory_path}/node_modules/.bin/backtrace-js"
 
 # run backtrace-js on bundle
 "$NODE_BINARY" "$backtrace_js_path" upload \
