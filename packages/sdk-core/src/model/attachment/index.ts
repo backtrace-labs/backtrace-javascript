efb9d91502f1c2ebffda48e564e0b584269d69bc
Sebastian Alex <sebastian.alex@saucelabs.com>
2025-06-27 11:50:10 +0200

    sdk-core: remove file attachments [INT-355]

--- packages/sdk-core/src/model/attachment/index.ts
+++ packages/sdk-core/src/model/attachment/index.ts
@@ -1,4 +1,3 @@
 export * from './BacktraceAttachment.js';
-export * from './BacktraceFileAttachment.js';
 export * from './BacktraceStringAttachment.js';
 export * from './BacktraceUint8ArrayAttachment.js';
--- packages/sdk-core/src/modules/attachments/isFileAttachment.ts
+++
@@ -1,5 +0,0 @@
-import { BacktraceAttachment, BacktraceFileAttachment } from '../../model/attachment/index.js';
-
-export function isFileAttachment(attachment: BacktraceAttachment): attachment is BacktraceFileAttachment {
-    return 'filePath' in attachment && typeof attachment.filePath === 'string';
-}
