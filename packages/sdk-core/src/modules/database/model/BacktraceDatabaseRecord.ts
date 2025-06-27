c7b614934fcb8d9231bf5a20ef827e8d5a554c7a
Sebastian Alex <sebastian.alex@saucelabs.com>
2025-06-27 13:06:06 +0200

    sdk-core: fix endless loop when loading SessionFiles

--- packages/sdk-core/src/modules/database/model/BacktraceDatabaseRecord.ts
+++ packages/sdk-core/src/modules/database/model/BacktraceDatabaseRecord.ts
@@ -1,12 +1,13 @@
 import { BacktraceAttachment } from '../../../model/attachment/index.js';
 import { BacktraceData } from '../../../model/data/BacktraceData.js';
+import { SessionId } from '../../storage/SessionFiles.js';
 
 export interface ReportBacktraceDatabaseRecord {
     readonly type: 'report';
     readonly data: BacktraceData;
     readonly id: string;
     readonly timestamp: number;
-    readonly sessionId?: string;
+    readonly sessionId?: SessionId;
     attachments: BacktraceAttachment[];
     /**
      * Determines if the record is in use
@@ -20,7 +21,7 @@ export interface AttachmentBacktraceDatabaseRecord {
     readonly rxid: string;
     readonly timestamp: number;
     readonly attachment: BacktraceAttachment;
-    readonly sessionId: string;
+    readonly sessionId: SessionId;
     /**
      * Determines if the record is in use
      */
