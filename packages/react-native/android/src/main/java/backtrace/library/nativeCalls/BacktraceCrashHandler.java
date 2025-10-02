4d18e22a1c86771a2ba7a4b08a6f68e8ac87f731
melekr <melekramki@gmail.com>
2025-10-02 19:10:54 -0400

    Set JavaCrashHandler as default
    Remove handler executable

--- packages/react-native/android/src/main/java/backtrace/library/nativeCalls/BacktraceCrashHandler.java
+++ packages/react-native/android/src/main/java/backtrace/library/nativeCalls/BacktraceCrashHandler.java
@@ -5,9 +5,4 @@ public class BacktraceCrashHandler {
 
     public static native boolean initializeJavaCrashHandler(String url, String databasePath, String classPath, String[] attributeKeys, String[] attributeValues,
                                                             String[] attachmentPaths, String[] environmentVariables);
-
-    public static native boolean initializeCrashHandler(String url, String databasePath, String handlerPath,
-                                                        String[] attributeKeys, String[] attributeValues,
-                                                        String[] attachmentPaths, boolean enableClientSideUnwinding,
-                                                        Integer unwindingMode);
 }
