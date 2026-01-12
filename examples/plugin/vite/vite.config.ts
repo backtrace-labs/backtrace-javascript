c6fa54a9f554f21a12480ca08ca2ab09f8e3a070
Adam Cronin <acronin@backtrace.io>
2026-01-12 12:41:07 -0500

    Running prettier on the new example code

--- examples/plugin/vite/vite.config.ts
+++ examples/plugin/vite/vite.config.ts
@@ -2,20 +2,20 @@ import { defineConfig, loadEnv } from 'vite';
 import { BacktracePlugin } from '@backtrace/vite-plugin';
 
 export default defineConfig(({ mode }) => {
-  // Load env variables from .env file
-  const env = loadEnv(mode, process.cwd(), '');
+    // Load env variables from .env file
+    const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    build: {
-      sourcemap: true,
-    },
-    plugins: [
-      BacktracePlugin({
-        uploadUrl: env.BACKTRACE_UPLOAD_URL,
-        uploadOptions: {
-          includeSources: true,
+    return {
+        build: {
+            sourcemap: true,
         },
-      }),
-    ],
-  };
+        plugins: [
+            BacktracePlugin({
+                uploadUrl: env.BACKTRACE_UPLOAD_URL,
+                uploadOptions: {
+                    includeSources: true,
+                },
+            }),
+        ],
+    };
 });
