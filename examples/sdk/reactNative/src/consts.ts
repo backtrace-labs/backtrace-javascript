ee0b152cc950452fac18f5a83cc30ecb37853093
Bartosz Litwiniuk <>
2026-06-23 00:46:22 +0200

    react-native: Bump dependencies

--- examples/sdk/reactNative/src/consts.ts
+++ examples/sdk/reactNative/src/consts.ts
@@ -1,2 +1,2 @@
 export const SUBMISSION_URL =
-    'https://submit.backtrace.io/your-universe/0000000000000000000000000000000000000000000000000000000000000000/json';
+    'https://submit.backtrace.io/yolo/4c4a3ce48334849fdba6e0c24066dbed870ff2df93bc29f6aee0aaa9d4e05c97/json';
